a968b77e059d93a8c345f3c2fc070fb647bc0308
dwiliandy <email_address>
2022-02-23 16:53:46 +0700

    Fix Login View

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,34 +1,26 @@
-<x-guest-layout>
-    <x-auth-card>
+@extends('layouts.auth.app',[
+  'title' =>'Login'
+])
+@section('content')
+<div class="row justify-content-center">
+  <div class="col-md-4">
+    <main class="form-signin">
+      <form method="POST" action="{{ route('login') }}">
+       @csrf
+          <img class="mb-4 img-fluid  mx-auto d-block" src="{{ asset('sion-1.jfif') }}" alt="" width="200" height="200">
+        <div class="form-floating">
+          <input type="email" class="form-control" name="email" id="floatingInput" placeholder="name@example.com">
+          <label for="floatingInput">Email address</label>
+        </div>
+        <div class="form-floating">
+          <input type="password" class="form-control" name="password" id="floatingPassword" placeholder="Password">
+          <label for="floatingPassword">Password</label>
+        </div>
+        <button class="w-100 btn btn-lg btn-login" type="submit">Login</button>
+        <p class="mt-5 mb-3 text-muted text-center">&copy; 2021-{{ now()->year }}</p>
+      </form>
+    </main>
+  </div>
+</div>
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-input id="email" placeholder="Email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                placeholder="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
-
-            <div class="text-center mt-4">
-                <x-button class="ml-3 align-center">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+@endsection
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -9,9 +9,9 @@
 
 </style>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #7952b3">
     <div class="container">
-        <a class="navbar-brand" href="#">SioNerz</a>
+        <a class="navbar-brand" href="/">SionerZ</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -21,26 +21,29 @@
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                         data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
+                        Konten
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><a class="dropdown-item" href="#">Renungan</a></li>
+                        <li><a class="dropdown-item" href="#">Artikel</a></li>
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
+                  <a class="nav-link" href="#">Struktur Organisasi</a>
                 </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                      data-bs-toggle="dropdown" aria-expanded="false">
+                      Berita dan Kegiatan
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li><a class="dropdown-item" href="#">Berita</a></li>
+                      <li><a class="dropdown-item" href="#">Kegiatan</a></li>
+                  </ul>
+              </li>
             </ul>
             @auth
             <a class="btn btn-outline-danger auth-btn" href="{{ route('logout') }}" onclick="event.preventDefault();
@@ -51,7 +54,7 @@
                 @csrf
             </form>
             @else
-            <a class="btn btn-outline-success auth-btn" href="{{ route('login') }}">{{ __('LOGIN') }}</a>
+            <a class="btn btn-outline-warning auth-btn" href="{{ route('login') }}">{{ __('LOGIN') }}</a>
             @endauth
         </div>
     </div>
