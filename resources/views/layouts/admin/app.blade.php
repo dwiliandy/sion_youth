d0e52e05f0c06e7a7b51fe4a439e37a1ee46adb5
dwiliandy <email_address>
2022-02-24 13:45:00 +0700

    template admin

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -10,12 +10,13 @@
     <title>
         {{ env('APP_NAME') }} | {{ $title }}
     </title>
+
     @yield('styles')
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    
+
     {{-- SB-admin --}}
     <link href="{{ asset('admin_template') }}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
@@ -26,19 +27,22 @@
     <link href="{{ asset('admin_template') }}/css/sb-admin-2.min.css" rel="stylesheet">
 </head>
 
-<body class="g-sidenav-show bg-gray-100">
-  <div class="wrapper">
-    @include('layouts.admin.top_bar')
-    <main>
-        <div class="container-fluid py-4">
-            @yield('content')
+<body id="page-top">
+
+    <div id="wrapper">
+        @include('layouts.admin.sidebar')
+        <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content">
+                @include('layouts.admin.topbar')
+                @yield('content')
+            </div>
+            @include('layouts.admin.footer')
         </div>
-    </main>
-  </div>
-    
+    </div>
+
     <!-- Scroll to Top Button-->
     <a class="scroll-to-top rounded" href="#page-top">
-      <i class="fas fa-angle-up"></i>
+        <i class="fas fa-angle-up"></i>
     </a>
 
     <!-- Logout Modal-->
@@ -55,15 +59,18 @@
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
+                    <a class="btn btn-primary" href="{{ route('logout') }}" onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
                 </div>
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
-    
+
     <!-- Bootstrap core JavaScript-->
     <script src="{{ asset('admin_template') }}/vendor/jquery/jquery.min.js"></script>
     <script src="{{ asset('admin_template') }}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -73,13 +80,6 @@
 
     <!-- Custom scripts for all pages-->
     <script src="{{ asset('admin_template') }}/js/sb-admin-2.min.js"></script>
-
-    <!-- Page level plugins -->
-    <script src="{{ asset('admin_template') }}/vendor/chart.js/Chart.min.js"></script>
-
-    <!-- Page level custom scripts -->
-    <script src="{{ asset('admin_template') }}/js/demo/chart-area-demo.js"></script>
-    <script src="{{ asset('admin_template') }}/js/demo/chart-pie-demo.js"></script>
 
 </body>
 
